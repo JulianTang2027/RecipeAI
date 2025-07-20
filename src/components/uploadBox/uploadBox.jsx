93d36b685eb3aaa7de93c0cf47d19cceb4b497a9
JulianTang2027 <juliantang2027@u.northwestern.edu>
2025-07-20 18:27:20 +0800

    Changed button to input

--- src/components/uploadBox/uploadBox.jsx
+++ src/components/uploadBox/uploadBox.jsx
@@ -3,11 +3,18 @@ import styles from './uploadBox.module.css';
 import upload from '../../assets/upload.svg';
 
 const UploadBox = () => {
+    const [link, setLink] = useState("");
+
+    const handleInputChange = (e) => {
+        setLink(e.target.value);
+        console.log(link);
+    }
+
     return (
         <div className={styles.uploadBox}>
             <div className={styles.uploadSegment}>
-                <span>Upload a link to the tiktok or ig reel here!</span>
-                <button className={styles.uploadButton}>📤</button>
+                <label className={styles.uploadButton} for="uploadInput">Upload a link to the tiktok or ig reel here!</label>
+                <input type="text" id="uploadInput" onChange={handleInputChange}></input>
             </div>
         </div>
     )
